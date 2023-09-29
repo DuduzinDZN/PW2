1e83052d6326f577d941897622b11d40b443ec84
DuduzinDZN <luiseduardodcf@gmail.com>
2023-09-28 22:19:33 -0300

    Primeiro commit

---
+++ WebApplication1/Controllers/AlunoController.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class AlunoController : Controller
+    {
+        // GET: Aluno
+        public ActionResult Index()
+        {
+            return View(AlunoModel.CriarLista());
+        }
+
+        // GET: Aluno/Details/5
+        public ActionResult Details(int id)
+        {
+            return View(AlunoModel.CriarLista()[id]);
+        }
+
+        // GET: Aluno/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Aluno/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Creat()
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View(new AlunoModel());
+            }
+        }
+
+        // GET: Aluno/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Aluno/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Aluno/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View(AlunoModel.CriarLista()[id]);
+        }
+
+        // POST: Aluno/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ WebApplication1/Models/AlunoModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class AlunoModel
+    {
+        public String Nome { get; set; }
+        public string Curso { get; set; }
+        public DateTime Matricula { get; set; }
+        public int Faltas { get; set; }
+
+        public AlunoModel()
+        {
+            Nome = "Nicolas";
+            Curso = "DS1";
+            Matricula = new DateTime(2023, 1, 1);
+            Faltas = 10;
+        }
+
+        public static AlunoModel CriarAluno()
+        {
+            var aluno = new AlunoModel();
+            aluno.Nome = "Emerson";
+
+            return aluno;
+
+        }
+        public static List<AlunoModel> CriarLista()
+        {
+            var lista = new List<AlunoModel>();
+            lista.Add(new AlunoModel());
+            lista.Add(AlunoModel.CriarAluno());
+
+            return lista;
+        }
+        
+ 
+
+    }
+
+}
